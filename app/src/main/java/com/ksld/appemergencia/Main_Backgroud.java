04aad193154cfb64417163ef228bb86f4fdce792
Leonel Parrales <dparrales@est.ups.edu.ec>
2021-02-10 16:50:02 -0500

    Update Main_Backgroud.java

--- app/src/main/java/com/ksld/appemergencia/Main_Backgroud.java
+++ app/src/main/java/com/ksld/appemergencia/Main_Backgroud.java
@@ -10,11 +10,13 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -145,6 +147,11 @@ public class Main_Backgroud extends AppCompatActivity {
         });
     }
 
+    //***********************************//
+    //***********************************//
+            //Imagen
+    //***********************************//
+    //***********************************//
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -152,13 +159,37 @@ public class Main_Backgroud extends AppCompatActivity {
         if (requestCode == 100) {
             // Get Capture Image
             captureImage = (Bitmap) data.getExtras().get("data"); //Variable que guarda la imagen
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            captureImage.compress(Bitmap.CompressFormat.JPEG,100, stream);
-            byte[] byteArray = stream.toByteArray();
+
+            // BITARRAY
+            byte[] bitarray=getImageByte(captureImage);
+
+
+            //ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            //captureImage.compress(Bitmap.CompressFormat.JPEG,100, stream);
+            //byte[] byteArray = stream.toByteArray();
             //https://stackoverflow.com/questions/20329090/how-to-convert-a-bitmap-to-a-jpeg-file-in-android/20329141
         }
     }
 
+    //For encoding toString
+    public byte[] getImageByte(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        //String encodedImage = android.util.Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        //return encodedImage;
+        return imageBytes;
+    }
+
+
+
+
+
+    //***********************************//
+    //***********************************//
+    //Imagen
+    //***********************************//
+    //***********************************//
 
 
     @Override
